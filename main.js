dfa5b57a706b2e1f89b43aca924cc1b77ec56b63
Karol Filas <filaskarol@gmail.com>
2023-03-22 21:36:52 +0100

    finished project

---
+++ main.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector('h1')
+
+document.body.addEventListener('mousemove', (e) => {
+    // console.log(e.clientX, e.clientY);
+    h1.textContent = `${e.clientX}, ${e.clientY}`
+})
+
+
